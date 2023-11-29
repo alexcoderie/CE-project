2deddbaa081c93823134aff6d15133bad6425753
Alexandru Coderie <alexcoderie5@gmail.com>
2023-11-29 19:38:29 +0200

    Commited the base structure for the project

---
+++ Memory.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project
+{
+    public struct Memory
+    {
+        //(16 bit words as signed)
+        short[] MemData;
+
+        public void Init()
+        {
+            MemData = new short[400];
+
+            for(int i = 0; i < 400; i++)
+            {
+                MemData[i] = 0;
+            }
+
+            MemData[0] = 0b0111000100000010;
+            MemData[1] = 0b0111000100010010;
+            MemData[2] = 0b0111000100100010;
+            MemData[3] = 0b0111000100110010;
+            MemData[4] = 0b0111000110000010;
+        }
+
+        public void ViewMem() //!!! Works, however it needs some "pretty printing" BS...
+        {
+            for (byte i = 0; i <= 39; i++)
+            {
+                for (byte j = 0; j <= 9; j++)
+                {
+                    Console.Write(MemData[((i*10)+j)] + " ");
+                    //Console.Write((i * 10) + j + " "); //Testing 40x10 grid style printing
+                }
+                Console.WriteLine("");
+            }
+        }
+
+        public short ExtractMem(short PC)
+        {
+            //Console.WriteLine(PC);
+            return MemData[PC];
+        }
+    }
+}
